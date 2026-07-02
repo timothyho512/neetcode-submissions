6f21fc84650cccabca8fdd0f3bc52f87d42a76e5
Timothy Ho <timothyho512@gmail.com>
2026-07-02 19:00:53 +0100

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # is balcned if height different is no more than 1
+
+        # definitely dfs here
+
+        # i think again same as last question
+
+        # i definitely need a helper function here because I need the height from the left and right node from the root
+
+
+        self.ans = True
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if abs(left - right) > 1:
+                self.ans = False
+            return 1 + max(left, right)
+        
+
+ 
+        dfs(root)
+
+        return self.ans
