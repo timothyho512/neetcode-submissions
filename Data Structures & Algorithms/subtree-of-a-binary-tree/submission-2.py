15e2cdc2a9dd6160fa6f0093ebbb821f82dbf49e
Timothy Ho <timothyho512@gmail.com>
2026-07-10 12:06:56 +0100

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        # so we need to bascailly go through the whole root and compare to the subRoot
+
+        # so there are a few things
+
+        #if the currrent node is the same, you go left and right together
+
+        # if not you go left and right, while the subtree has to reset and point to thr root once again right
+
+        # i am thinking if i need a helper functions here, 
+
+        # i think this seems that i need a helper function no?
+
+        # acutally i am not sure, no helper function because it returns bool which is what i need
+
+        # yes helper function because i can save the answer in the global variables right (do i need it?)
+
+        def isSameTree(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+        # base case:
+        if not root or not subRoot:
+            return False
+
+        if root.val == subRoot.val:
+            if isSameTree(root, subRoot):
+                return True
+        
+        left = self.isSubtree(root.left, subRoot)
+        right = self.isSubtree(root.right, subRoot)
+
+   
+        if left or right:
+            return True
+        return False
